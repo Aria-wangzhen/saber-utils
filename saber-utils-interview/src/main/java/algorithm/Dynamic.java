a22477ad184f451033113c45de0464be5b85e37c
wangzhen <wangzhen36@meituan.com>
2018-12-24 10:06:28 +0800

    pom

--- saber-utils-interview/src/main/java/algorithm/Dynamic.java
+++ saber-utils-interview/src/main/java/algorithm/Dynamic.java
@@ -15,10 +15,19 @@ public class Dynamic {
         //int[][] tower = {{13}, {11, 8}, {12, 7, 26}, {6, 14, 15, 8}, {12, 7, 13, 24, 11}};
         //int result = dataTower(tower);
         //editDistance();
-        matrixFetch();
+        //matrixFetch();
+
+        //背包问题
+        int m = 10;
+        int n = 3;
+        int w[] = {3, 4, 5};
+        int p[] = {4, 5, 6};
+        int c = BackPack_Solution(m, n, w, p);
+        System.out.println(c);
     }
 
     /**
+     * 数塔取数
      * 来源：https://blog.csdn.net/tterminator/article/details/50951137?utm_source=blogxgwz3
      *
      * @param tower
@@ -91,7 +100,7 @@ public class Dynamic {
                     //修改，删除，插入的代价
                     dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
-                System.out.println( dp[i][j]);
+                System.out.println(dp[i][j]);
             }
         }
         System.out.println(dp[aLen][bLen]);
@@ -120,46 +129,44 @@ public class Dynamic {
         System.out.println(dp[n]);
     }
 
-
     /**
      * 背包问题
      * 参考：https://www.cnblogs.com/lfeng1205/p/5981198.html
      *
-     * @return
-     */
-    /**
      * @param m 表示背包的最大容量
      * @param n 表示商品个数
      * @param w 表示商品重量数组
      * @param p 表示商品价值数组
      */
     public static int BackPack_Solution(int m, int n, int[] w, int[] p) {
-        //c[i][v]表示前i件物品恰放入一个重量为m的背包可以获得的最大价值
-        int c[][] = new int[n + 1][m + 1];
+        //value[i][v]表示前i件物品恰放入一个重量为m的背包可以获得的最大价值
+        int value[][] = new int[n + 1][m + 1];
+        //行初始化
         for (int i = 0; i < n + 1; i++) {
-            c[i][0] = 0;
+            value[i][0] = 0;
         }
+        //列初始化
         for (int j = 0; j < m + 1; j++) {
-            c[0][j] = 0;
+            value[0][j] = 0;
         }
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                //当物品为i件重量为j时，如果第i件的重量(w[i-1])小于重量j时，c[i][j]为下列两种情况之一：
-                //(1)物品i不放入背包中，所以c[i][j]为c[i-1][j]的值
-                //(2)物品i放入背包中，则背包剩余重量为j-w[i-1],所以c[i][j]为c[i-1][j-w[i-1]]的值加上当前物品i的价值
+                //当物品为i件重量为j时，如果第i件的重量(w[i-1])小于重量j时，value[i][j]为下列两种情况之一：
+                //(1)物品i不放入背包中，所以value[i][j]为value[i-1][j]的值
+                //(2)物品i放入背包中，则背包剩余重量为j-w[i-1],所以value[i][j]为value[i-1][j-w[i-1]]的值加上当前物品i的价值
                 if (w[i - 1] <= j) {
-                    if (c[i - 1][j] < (c[i - 1][j - w[i - 1]] + p[i - 1])) {
-                        c[i][j] = c[i - 1][j - w[i - 1]] + p[i - 1];
+                    if (value[i - 1][j] < (value[i - 1][j - w[i - 1]] + p[i - 1])) {
+                        value[i][j] = value[i - 1][j - w[i - 1]] + p[i - 1];
                     } else {
-                        c[i][j] = c[i - 1][j];
+                        value[i][j] = value[i - 1][j];
                     }
                 } else {
-                    c[i][j] = c[i - 1][j];
+                    value[i][j] = value[i - 1][j];
                 }
             }
         }
-        return c[n][m];
+        return value[n][m];
     }
 
 
