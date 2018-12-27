fe6c4849d96a3a99da4e8a8929499adeff80adca
wangzhen <wangzhen36@meituan.com>
2018-12-27 22:20:49 +0800

    pom

--- saber-utils-interview/src/main/java/zuoChengYun/chapter_5_stringproblem/Problem_15_ExpressionCompute.java
+++ saber-utils-interview/src/main/java/zuoChengYun/chapter_5_stringproblem/Problem_15_ExpressionCompute.java
@@ -12,22 +12,28 @@ public class Problem_15_ExpressionCompute {
     }
 
     public static int[] value(char[] str, int i) {
+        //存放以前累计结果和当前数字最近的运算符
         LinkedList<String> que = new LinkedList<String>();
         int pre = 0;
+        //bra括号的意思
         int[] bra = null;
         while (i < str.length && str[i] != ')') {
+            // 0 - 9 数字处理
             if (str[i] >= '0' && str[i] <= '9') {
                 pre = pre * 10 + str[i++] - '0';
             } else if (str[i] != '(') {
+                //不是数字和左括号进去处理
                 addNum(que, pre);
                 que.addLast(String.valueOf(str[i++]));
                 pre = 0;
             } else {
+                //左括号处理
                 bra = value(str, i + 1);
                 pre = bra[0];
                 i = bra[1] + 1;
             }
         }
+        //右括号处理
         addNum(que, pre);
         return new int[]{getNum(que), i};
     }
@@ -66,10 +72,10 @@ public class Problem_15_ExpressionCompute {
     }
 
     public static void main(String[] args) {
-        String exp = "48*((70-65)-43)+8*1";
+        String exp = "4*((7*5)-43)+8*1";
         System.out.println(getValue(exp));
 
-        exp = "4*(6+78)+53-9/2+45*8";
+        /*exp = "4*(6+78)+53-9/2+45*8";
         System.out.println(getValue(exp));
 
         exp = "10-5*3";
@@ -79,7 +85,7 @@ public class Problem_15_ExpressionCompute {
         System.out.println(getValue(exp));
 
         exp = "3+1*4";
-        System.out.println(getValue(exp));
+        System.out.println(getValue(exp));*/
 
     }
 
