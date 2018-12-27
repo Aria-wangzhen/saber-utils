24f625a88d30cabcd40e04c0798fef962b241833
wangzhen <wangzhen36@meituan.com>
2018-12-27 20:12:51 +0800

    pom

--- saber-utils-interview/src/main/java/zuoChengYun/chapter_5_stringproblem/Problem_13_PalindromeString.java
+++ saber-utils-interview/src/main/java/zuoChengYun/chapter_5_stringproblem/Problem_13_PalindromeString.java
@@ -6,6 +6,15 @@ public class Problem_13_PalindromeString {
     /**
      * https://blog.csdn.net/afei__/article/details/83214042
      * 最长回文字符串
+     * 这道题本质是求字符串中的最大回文字串的长度
+     * 动态规划法，
+     * 假设dp[ i ][ j ]的值为true，表示字符串s中下标从 i 到 j 的字符组成的子串是回文串。那么可以推出：
+     * dp[ i ][ j ] = dp[ i + 1][ j - 1] && s[ i ] == s[ j ]。
+     * 这是一般的情况，由于需要依靠i+1, j -1，所以有可能 i + 1 = j -1, i +1 = (j - 1) -1，
+     * 因此需要求出基准情况才能套用以上的公式：
+     * a. i + 1 = j -1，即回文长度为1时，dp[ i ][ i ] = true;
+     * b. i +1 = (j - 1) -1，即回文长度为2时，dp[ i ][ i + 1] = （s[ i ] == s[ i + 1]）。
+     * 有了以上分析就可以写出代码了。需要注意的是动态规划需要额外的O(n^2)的空间。
      *
      * @param s
      * @return
@@ -24,7 +33,7 @@ public class Problem_13_PalindromeString {
             for (int j = 0; j <= i; j++) {
                 if (i - j < 2) {
                     // 子字符串长度小于 2 的时候单独处理
-                    lps[j][i] = chars[i] == chars[j];
+                    lps[j][i] = (chars[i] == chars[j]);
                 } else {
                     // 如果 [i, j] 是回文子串，那么一定有 [j+1, i-1] 也是回子串
                     lps[j][i] = lps[j + 1][i - 1] && (chars[i] == chars[j]);
@@ -39,6 +48,40 @@ public class Problem_13_PalindromeString {
         return s.substring(start, start + maxLen);
     }
 
+    private static int longestPalindrome(String s) {
+
+        // 不考虑非法输入的情况，比如null
+        int maxLen = 0;
+        int len = s.length();
+        boolean[][] t = new boolean[len][len];
+
+        // 单个字符串都是回文
+        for (int i = 0; i < len; i++) {
+            t[i][i] = true;
+            maxLen = 1;
+        }
+
+        for (int i = 0; i < len - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                t[i][i + 1] = true;
+                maxLen = 2;
+            }
+        }
+        // 求长度大于2的子串是否是回文串
+        for (int gap = 3; gap <= len; gap++) {
+            for (int i = 0, j; (j = i + gap - 1) <= len - 1; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    t[i][j] = t[i + 1][j - 1];
+                    if (t[i][j] && gap > maxLen) {
+                        maxLen = gap;
+                    }
+                } else {
+                    t[i][j] = false;
+                }
+            }
+        }
+        return maxLen;
+    }
 
     /**
      * 添加最少字符使字符串整体都是回文字符串
