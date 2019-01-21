38f2c7aa3a88190245beecbed85e19ab98203a5a
wangzhen <wangzhen36@meituan.com>
2019-01-21 12:04:06 +0800

    动态代理

--- saber-utils-interview/src/main/java/ZPractise/Practise.java
+++ saber-utils-interview/src/main/java/ZPractise/Practise.java
@@ -996,6 +996,34 @@ public class Practise {
         return isBalance(root.left) && isBalance(root.right);
     }
 
+
+    /**
+     * 思路：https://blog.csdn.net/u013276277/article/details/78575033
+     * 11.判断二叉树是否是平衡二叉树
+     */
+    private boolean isBalanced = true;//最后的返回值
+    //后续遍历时，遍历到一个节点，其左右子树已经遍历  依次自底向上判断，每个节点只需要遍历一次
+    public boolean IsBalanced_Solution(TreeNode root) {
+        getDepth(root);
+        return isBalanced;
+    }
+
+    public int getDepth(TreeNode root) {
+        if (root == null) {
+            isBalanced = true;
+            return 0;
+        }
+        int left = getDepth(root.left);//左子树
+        int right = getDepth(root.right);//右子树
+        int depth = left > right ? left + 1 : right + 1;
+        if (Math.abs(left - right) <= 1) {
+            isBalanced = true;
+        } else {
+            isBalanced = false;
+        }
+        return depth;//下层的深度，上层可以接着用免得再遍历
+    }
+
     /**
      * 12.二叉树第k层的节点个数 -- 递归
      */
--- saber-utils-interview/src/main/java/algorithm/leetCode/LC4_MedianofTwSortedArrays.java
+++ saber-utils-interview/src/main/java/algorithm/leetCode/LC4_MedianofTwSortedArrays.java
@@ -1,6 +1,9 @@
 package algorithm.leetCode;
 
 /**
+ * 4.寻找两个有序数组的中位数
+ * http://www.cnblogs.com/grandyang/p/4465932.html
+ *
  * @author Aria
  * @time on 2018-12-29.
  */
@@ -11,7 +14,7 @@ public class LC4_MedianofTwSortedArrays {
         //要找的数的位置
         int leftK = (m + n + 1) / 2;
         int rightK = (m + n + 2) / 2;
-        int value1 =  findKth(nums1, 0, nums2, 0, leftK);
+        int value1 = findKth(nums1, 0, nums2, 0, leftK);
         int value2 = findKth(nums1, 0, nums2, 0, rightK);
         return (value1 + value2) / 2.0;
     }
@@ -36,24 +39,25 @@ public class LC4_MedianofTwSortedArrays {
             return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
         }
     }
-    public int findK(int[] nums1, int i, int[] nums2, int j , int k) {
-        if(i>=nums1.length){
-            return nums2[j+k-1];
+
+    public int findK(int[] nums1, int i, int[] nums2, int j, int k) {
+        if (i >= nums1.length) {
+            return nums2[j + k - 1];
         }
-        if(j>=nums2.length){
-            return nums1[i+k-1];
+        if (j >= nums2.length) {
+            return nums1[i + k - 1];
         }
 
-        if(k == 1){
-            return Math.min(nums1[i],nums2[j]);
+        if (k == 1) {
+            return Math.min(nums1[i], nums2[j]);
         }
-        int value1 = ((i + k/2 -1) < nums1.length) ? nums1[i + k/2 -1] : Integer.MAX_VALUE;
-        int value2 = ((j + k/2 -1) < nums2.length) ? nums2[j + k/2 -1] : Integer.MAX_VALUE;
+        int value1 = ((i + k / 2 - 1) < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+        int value2 = ((j + k / 2 - 1) < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
 
-        if(value1 < value2){
-            return findK(nums1, i + k/2, nums2 ,j, k - k/2);
-        }else{
-            return findK(nums1, i, nums2 ,j + k/2, k - k/2);
+        if (value1 < value2) {
+            return findK(nums1, i + k / 2, nums2, j, k - k / 2);
+        } else {
+            return findK(nums1, i, nums2, j + k / 2, k - k / 2);
         }
 
     }
@@ -61,15 +65,13 @@ public class LC4_MedianofTwSortedArrays {
     public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int leftK = ( m + n + 1 )/2;
-        int righK = ( m + n + 2 )/2;
-        int value1 = findK(nums1, 0, nums2 ,0,leftK);
-        int value2 = findK(nums1, 0, nums2 ,0,righK);
-        return (value1+value2)/2;
+        int leftK = (m + n + 1) / 2;
+        int righK = (m + n + 2) / 2;
+        int value1 = findK(nums1, 0, nums2, 0, leftK);
+        int value2 = findK(nums1, 0, nums2, 0, righK);
+        return (value1 + value2) / 2;
 
     }
-
-
 
 
     public static void main(String[] args) {
@@ -80,7 +82,7 @@ public class LC4_MedianofTwSortedArrays {
         nums1[3] = 4;
         nums1[4] = 5;
         nums1[5] = 6;
-        nums1[6] =7;
+        nums1[6] = 7;
 
         int[] nums2 = new int[3];
         nums2[0] = 1;
